a7292990e67721809b35f2afbc5c16724e686622
Mega-Burger <woldepo@gmail.com>
2026-03-27 11:49:30 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
@@ -10,3 +10,14 @@ def read_root():
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
+
+@app.get("/api/ip")
+async def get_ip(request: Request):
+    x_forwarded_for = request.headers.get("x-forwarded-for")
+
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(",")[0]
+    else:
+        ip = request.client.host
+
+    return {"ip": ip}
