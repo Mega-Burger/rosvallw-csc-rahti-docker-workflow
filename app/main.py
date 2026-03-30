95655fbe8c43c860c058ce3331b984f273aa2f2e
Mega-Burger <woldepo@gmail.com>
2026-03-30 10:03:51 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins=["*"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
